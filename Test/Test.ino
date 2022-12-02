18b774ec0ec71d52b89c8ff944d8ad15a94d7932
Esteb37 <esteban37padilla@gmail.com>
2022-12-01 23:18:42 -0600

    Send one by one

--- Test/Test.ino
+++ Test/Test.ino
@@ -5,11 +5,11 @@
 #define MIC A0
 
 // Wifi security
-const char *ssid = "Hilda";
-const char *password = "hildab04";
+const char *ssid = "MotoEPC";
+const char *password = "";
 
 // MQTT Broker IP address
-const char *mqtt_server = "172.20.10.2";
+const char *mqtt_server = "192.168.135.36";
 // const char* mqtt_server = "10.25.18.8";
 
 WiFiClient espClient;
@@ -24,78 +24,78 @@ const int ledPin = 2;
 
 uint16_t adc;
 
-
 void setup()
 {
-    Serial.begin(9600);
-    Serial.println("Starting");
-    setup_wifi();
-    client.setServer(mqtt_server, 1883);
-    start = millis();
+  Serial.begin(9600);
+  Serial.println("Starting");
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  start = millis();
 }
 
 void setup_wifi()
 {
-    delay(10);
-    // connecting to a WiFi network
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
+  delay(10);
+  // connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-    WiFi.begin(ssid, password);
+  WiFi.begin(ssid, password);
 
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print(".");
-    }
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
 
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void reconnect()
 {
-    // Reconnect
-    while (!client.connected())
-    {
-        Serial.print("Attempting MQTT connection...");
-        // Attempt to connect
-        if (client.connect("ESP32Client_rojo"))
-        { //"ESPClient_3" represent the client name that connects to broker
-            Serial.println("connected");
-        }
-        else
-        {
-            Serial.print("failed, rc=");
-            Serial.print(client.state());
-            Serial.println(" try again in 5 seconds");
-            // Wait 5 seconds before retrying
-            delay(5000);
-        }
+  // Reconnect
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32Client_rojo"))
+    { //"ESPClient_3" represent the client name that connects to broker
+      Serial.println("connected");
     }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
 
 void loop()
 {
-    if (!client.connected())
-    {
-        reconnect();
-    }
-    client.loop();
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
 
-    adc = analogRead(MIC);
-    client.beginPublish("esp32/Mic", 2, false);
-    client.write((uint8_t *)&adc, 2);
-    client.endPublish();
+  adc = analogRead(MIC);
+  client.beginPublish("esp32/Mic", 2, false);
+  client.write((uint8_t *)&adc, 2);
+  client.endPublish();
 
-    counter++;
+  counter++;
 
-    if(millis()-start>1000){
-      Serial.println(counter);
-      counter = 0;
-      start = millis();
-    }
+  if (millis() - start > 1000)
+  {
+    Serial.println(counter);
+    counter = 0;
+    start = millis();
+  }
 }
